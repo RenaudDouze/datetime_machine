ac3190f09749216b8effbec94a8b7942f7c720a2
Renaud LEROGNON <rler@scan002.linkingup.org>
2019-04-24 08:29:19 +0200

    :white_check_mark: Add comments and proper the code

--- tests/DateTimeMachineTest.php
+++ tests/DateTimeMachineTest.php
@@ -10,160 +10,164 @@ use RenaudDouze\DateTimeMachine\DateTimeMachine;
  */
 final class DateTimeMachineTest extends TestCase
 {
-	/**
-	 * Test that the DateTimeMachine always return a \DateTime
-	 */
-	public function testReturnDatetime()
-	{
-		$machineDatetime = DateTimeMachine::when();
-
-		$this->assertEquals('DateTime', get_class($machineDatetime));
-		$this->assertNotEquals('RenaudDouze\DateTimeMachine\DateTimeMachine', get_class($machineDatetime));
-
-		$machineDatetime = DateTimeMachine::travel('now');
-
-		$this->assertEquals('DateTime', get_class($machineDatetime));
-		$this->assertNotEquals('RenaudDouze\DateTimeMachine\DateTimeMachine', get_class($machineDatetime));
-
-		$machineDatetime = DateTimeMachine::goBack();
-
-		$this->assertEquals('DateTime', get_class($machineDatetime));
-		$this->assertNotEquals('RenaudDouze\DateTimeMachine\DateTimeMachine', get_class($machineDatetime));
-	}
-	/**/
-
-	/**
-	 * Test that time travel from now to now still get you to now
-	 */
-	public function testComparaisonsBasic()
-	{
-		$machineDatetime = DateTimeMachine::when();
-		$vanillaDateTime = new \Datetime();
-
-		$this->assertEquals($machineDatetime->getTimestamp(), $vanillaDateTime->getTimestamp());
-
-		$machineDatetime = DateTimeMachine::travel('now');
-		$vanillaDateTime = new \Datetime();
-
-		$this->assertEquals($machineDatetime->getTimestamp(), $vanillaDateTime->getTimestamp());
-
-		$machineDatetime = DateTimeMachine::goBack();
-		$vanillaDateTime = new \Datetime();
-
-		$this->assertEquals($machineDatetime->getTimestamp(), $vanillaDateTime->getTimestamp());
-	}
-	/**/
-
-	/**
-	 * Travel to a date and check that we are
-	 */
-	public function testWhenAreWe()
-	{
-		$vanillaDateTime = new \Datetime('October 25, 1985');
-
-		$machineDatetimeFirst = DateTimeMachine::travel('October 25, 1985');
-
-		$this->assertEquals($machineDatetimeFirst->getTimestamp(), $vanillaDateTime->getTimestamp());
-
-		$machineDatetimeSecond = DateTimeMachine::whenAreWe();
-
-		$this->assertEquals($machineDatetimeSecond->getTimestamp(), $vanillaDateTime->getTimestamp());
-	}
-	/**/
-
-	/**
-	 *
-	 */
-	public function testWhen()
-	{
-		$vanillaDateTime = new \Datetime('October 25, 1985');
-
-		$machineDatetimeFirst = DateTimeMachine::travel('October 25, 1985');
-
-		$this->assertEquals($machineDatetimeFirst->getTimestamp(), $vanillaDateTime->getTimestamp());
-
-		$machineDatetimeSecond = DateTimeMachine::when();
-
-		$this->assertEquals($machineDatetimeSecond->getTimestamp(), $vanillaDateTime->getTimestamp());
-	}
-	/**/
-
-	/**
-	 *
-	 */
-	public function testATravelForNothing()
-	{
-		$vanillaDateTime = new \Datetime('October 25, 1985');
-
-		$machineDatetimeFirst = DateTimeMachine::travel('October 25, 1985');
-
-		$this->assertEquals($machineDatetimeFirst->getTimestamp(), $vanillaDateTime->getTimestamp());
-
-		$machineDatetimeSecond = DateTimeMachine::travel('now');
-
-		$this->assertEquals($machineDatetimeSecond->getTimestamp(), $vanillaDateTime->getTimestamp());
-	}
-	/**/
-
-	/**
-	 *
-	 */
-	public function testGoBack()
-	{
-		$now = new \Datetime();
-		$vanillaDateTime = new \Datetime('October 25, 1985');
-
-		$machineDatetimeFirst = DateTimeMachine::travel('October 25, 1985');
-
-		$this->assertEquals($machineDatetimeFirst->getTimestamp(), $vanillaDateTime->getTimestamp());
-
-		$machineDatetimeSecond = DateTimeMachine::goBack();
-
-		$this->assertEquals($machineDatetimeSecond->getTimestamp(), $now->getTimestamp());
-	}
-	/**/
-
-	/**
-	 *
-	 */
-	public function testWhenFirst()
-	{
-		$now = new \Datetime();
-		$vanillaDateTime = new \Datetime('October 25, 1985');
-
-		$machineDatetimeFirst = DateTimeMachine::when();
-
-		$this->assertEquals($machineDatetimeFirst->getTimestamp(), $now->getTimestamp());
-		$this->assertNotEquals($machineDatetimeFirst->getTimestamp(), $vanillaDateTime->getTimestamp());
-	}
-	/**/
-
-	/**
-	 * @expectedException \RunTimeException
-	 */
-	public function testGoBackFirst()
-	{
-		$machineDatetimeFirst = DateTimeMachine::goBack();
-	}
-	/**/
-
-	/**
-	 * @expectedException \RunTimeException
-	 */
-	public function testConstruct()
-	{
-		$datetime = new DateTimeMachine();
-	}
-	/**/
-
-	/**
-	 * Set up
-	 */
-	protected function setUp()
+    /**
+     * Test that the DateTimeMachine always return a \DateTime
+     */
+    public function testReturnDatetime()
     {
-    	if ('' !== session_id()) {
-			session_reset();
-		}
+        $machineDatetime = DateTimeMachine::when();
 
-	}
+        $this->assertEquals('DateTime', get_class($machineDatetime));
+        $this->assertNotEquals('RenaudDouze\DateTimeMachine\DateTimeMachine', get_class($machineDatetime));
+
+        $machineDatetime = DateTimeMachine::travel('now');
+
+        $this->assertEquals('DateTime', get_class($machineDatetime));
+        $this->assertNotEquals('RenaudDouze\DateTimeMachine\DateTimeMachine', get_class($machineDatetime));
+
+        $machineDatetime = DateTimeMachine::goBack();
+
+        $this->assertEquals('DateTime', get_class($machineDatetime));
+        $this->assertNotEquals('RenaudDouze\DateTimeMachine\DateTimeMachine', get_class($machineDatetime));
+    }
+    /**/
+
+    /**
+     * Test that time-travel from now to now still get you to now
+     */
+    public function testComparaisonsBasic()
+    {
+        $machineDatetime = DateTimeMachine::when();
+        $vanillaDateTime = new \Datetime();
+
+        $this->assertEquals($machineDatetime->getTimestamp(), $vanillaDateTime->getTimestamp());
+
+        $machineDatetime = DateTimeMachine::travel('now');
+        $vanillaDateTime = new \Datetime();
+
+        $this->assertEquals($machineDatetime->getTimestamp(), $vanillaDateTime->getTimestamp());
+
+        $machineDatetime = DateTimeMachine::goBack();
+        $vanillaDateTime = new \Datetime();
+
+        $this->assertEquals($machineDatetime->getTimestamp(), $vanillaDateTime->getTimestamp());
+    }
+    /**/
+
+    /**
+     * Time-travel to a date and check that we are (via WhenAreWe method)
+     */
+    public function testWhenAreWe()
+    {
+        $vanillaDateTime = new \Datetime('October 25, 1985');
+
+        $dtmFirst = DateTimeMachine::travel('October 25, 1985');
+
+        $this->assertEquals($dtmFirst->getTimestamp(), $vanillaDateTime->getTimestamp());
+
+        $dtmSecond = DateTimeMachine::whenAreWe();
+
+        $this->assertEquals($dtmSecond->getTimestamp(), $vanillaDateTime->getTimestamp());
+    }
+    /**/
+
+    /**
+     * Time-travel to a date and check that we are (via When method)
+     */
+    public function testWhen()
+    {
+        $vanillaDateTime = new \Datetime('October 25, 1985');
+
+        $dtmFirst = DateTimeMachine::travel('October 25, 1985');
+
+        $this->assertEquals($dtmFirst->getTimestamp(), $vanillaDateTime->getTimestamp());
+
+        $dtmSecond = DateTimeMachine::when();
+
+        $this->assertEquals($dtmSecond->getTimestamp(), $vanillaDateTime->getTimestamp());
+    }
+    /**/
+
+    /**
+     * Time-travel to now after time-travelling, so time-travel to the same date
+     */
+    public function testATravelForNothing()
+    {
+        $vanillaDateTime = new \Datetime('October 25, 1985');
+
+        $dtmFirst = DateTimeMachine::travel('October 25, 1985');
+
+        $this->assertEquals($dtmFirst->getTimestamp(), $vanillaDateTime->getTimestamp());
+
+        $dtmSecond = DateTimeMachine::travel('now');
+
+        $this->assertEquals($dtmSecond->getTimestamp(), $vanillaDateTime->getTimestamp());
+    }
+    /**/
+
+    /**
+     * Test the go back
+     */
+    public function testGoBack()
+    {
+        $now = new \Datetime();
+        $vanillaDateTime = new \Datetime('October 25, 1985');
+
+        $dtmFirst = DateTimeMachine::travel('October 25, 1985');
+
+        $this->assertEquals($dtmFirst->getTimestamp(), $vanillaDateTime->getTimestamp());
+
+        $dtmSecond = DateTimeMachine::goBack();
+
+        $this->assertEquals($dtmSecond->getTimestamp(), $now->getTimestamp());
+    }
+    /**/
+
+    /**
+     * Test that when() method return the current datetime if there is no previous time-travel
+     */
+    public function testWhenFirst()
+    {
+        $now = new \Datetime();
+        $vanillaDateTime = new \Datetime('October 25, 1985');
+
+        $dtmFirst = DateTimeMachine::when();
+
+        $this->assertEquals($dtmFirst->getTimestamp(), $now->getTimestamp());
+        $this->assertNotEquals($dtmFirst->getTimestamp(), $vanillaDateTime->getTimestamp());
+    }
+    /**/
+
+    /**
+     * Test the exception when you try to go back and never time-travelled
+     *
+     * @expectedException \RunTimeException
+     */
+    public function testGoBackFirst()
+    {
+        DateTimeMachine::goBack();
+    }
+    /**/
+
+    /**
+     * Test when you try to instanciate a DateTimeMachine
+     *
+     * @expectedException \RunTimeException
+     */
+    public function testConstruct()
+    {
+        new DateTimeMachine();
+    }
+    /**/
+
+    /**
+     * Set up
+     */
+    protected function setUp()
+    {
+        if ('' !== session_id()) {
+            session_reset();
+        }
+
+    }
 }
